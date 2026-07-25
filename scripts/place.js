4cdbe678a5e978ba5847b9f3356df1ab044950d9
chrishonest <Hchristopher@byupathway.edu>
2026-07-25 22:31:41 +0100

    complete project

--- scripts/place.js
+++ scripts/place.js
@@ -1,35 +1,29 @@
-const currentYear = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified");
 
-currentYear.textContent = new Date().getFullYear();
+document.getElementById("currentyear").textContent =
+    new Date().getFullYear();
 
-lastModified.textContent =
+document.getElementById("lastModified").textContent =
     `Last Modified: ${document.lastModified}`;
+
+// Weather Values
 
 const temperature = 28;
 const windSpeed = 8;
 
 function calculateWindChill(temp, speed) {
-
     return (
         13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(speed, 0.16) +
-        0.3965 * temp * Math.pow(speed, 0.16)
+        (0.6215 * temp) -
+        (11.37 * Math.pow(speed, 0.16)) +
+        (0.3965 * temp * Math.pow(speed, 0.16))
     ).toFixed(1);
-
 }
 
-const windChill = document.querySelector("#windChill");
+const windChill = document.getElementById("windChill");
 
 if (temperature <= 10 && windSpeed > 4.8) {
-
     windChill.textContent =
         `${calculateWindChill(temperature, windSpeed)} °C`;
-
-}
-else {
-
+} else {
     windChill.textContent = "N/A";
-
 }
