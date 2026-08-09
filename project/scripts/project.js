244bcbc8c7751d3482e633d484b841ececed35ad
chrishonest <Hchristopher@byupathway.edu>
2026-08-09 17:00:01 +0100

    week 5 planning

---
+++ project/scripts/project.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        name: "Coconut Bread",
+        category: "Bread",
+        description: "A soft and flavorful loaf made with a rich coconut taste.",
+        price: 2300,
+        image: "images/coconut-bread.webp"
+    },
+    {
+        name: "Family Loaf",
+        category: "Bread",
+        description: "A large, fresh loaf made for sharing with the whole family.",
+        price: 2500,
+        image: "images/family-loaf.webp"
+    },
+    {
+        name: "Buns and Milk Bread",
+        category: "Bread",
+        description: "Soft, delicious bread suitable for breakfast and snacks.",
+        price: 2000,
+        image: "images/milk-bread.webp"
+    },
+    {
+        name: "Mini Loaf",
+        category: "Bread",
+        description: "A convenient smaller loaf of freshly baked bread.",
+        price: 1500,
+        image: "images/mini-loaf.webp"
+    }
+];
